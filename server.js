0d35790d2b524b3148e6be49ee1357db0c583086
Somerandomexampleperson <somerandomexampleperson@gmail.com>
2024-08-20 18:49:56 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public')); // Serve static files from the 'public' directory
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Broadcast message to all clients when a new notification is received
+    socket.on('sendNotification', (data) => {
+        io.emit('notification', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
